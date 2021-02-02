c8abc08be3c23f0878273c8ce1e7416589f7373b
MykeNuLeng <linford.mike@gmail.com>
2021-02-02 13:14:05 +0000

    User POST route api/users/ now saves a user to the mongoDB database, and salts the password with BCrypt.

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
-const User = require('../models/User')
+const User = require('../models/User');
+const BCrypt = require('bcryptjs');
 
 // @route     POST api/users
 // @desc      Register a user
@@ -16,14 +17,34 @@ router.post('/', [
         }); 
     } ),
     check('password', 'Please enter a password of at least 6 characters').isLength({ min: 6 })
-], (req, res) => {
+], async (req, res) => {
     const errors = validationResult(req)
 
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() })
     }
-    console.log(req.body);
-    res.send('User route');
+
+    const { username, password } = req.body;
+
+    try {
+        // Encrypt password using BCrypt
+
+        // Return jsonwebtoken
+       let user = new User({
+            username,
+            password
+        })
+
+        const salt = await BCrypt.genSalt(10);
+
+        user.password = await BCrypt.hash(password, salt);
+        await user.save();
+        res.send('User registered');
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server error');
+    };
 });
 
 module.exports = router;
